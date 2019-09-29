7e28b47f00afa2d56093f3271e2b669b5ce309e2
jm <jm@insight026.local>
2019-09-29 23:29:52 +0100

    trying different setup for highlight guidelines

--- init.el
+++ init.el
@@ -217,7 +217,7 @@
 
 (use-package highlight-indent-guides
   :ensure t
-  :config
+  :init
   (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
   (setq highlight-indent-guides-method 'column))
 
