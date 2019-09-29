c1c071c4728b061960cc5d28f5a5188b6e82ac0d
jm <jm@insight026.local>
2019-09-30 00:28:01 +0100

    commit an error

--- init.el
+++ init.el
@@ -209,8 +209,11 @@
 (use-package s
   :ensure t)
 
-(use-package pyvenv
+;; (use-package pyvenv
   :ensure t)
+
+
+another change here
 
 ;; brings ivy as dependency
 ;; (use-package find-file-in-project
