63f23f72261b4a96dd81db2c6fa63beb99657764
jm <jm@insight026.local>
2019-09-30 02:59:39 +0100

    fixing order of package archives

--- init.el
+++ init.el
@@ -70,15 +70,15 @@
 ;; setup package archives
 (require 'package)
 (setq package-archives
-      '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
-        ("MELPA STABLE" . "https://stable.melpa.org/packages/")
-        ("MELPA"        . "https://melpa.org/packages/")
-        ("ORG"          . "https://orgmode.org/elpa/"))
+      '(("MELPA STABLE" . "https://stable.melpa.org/packages/")
+        ("GNU ELPA"     . "https://elpa.gnu.org/packages/")
+        ("ORG"          . "https://orgmode.org/elpa/")
+        ("MELPA"        . "https://melpa.org/packages/"))
       package-archive-priorities
       '(("MELPA Stable" . 10)
         ("GNU ELPA"     . 5)
-        ("MELPA"        . 0)
-        ("ORG"          . 3)))
+        ("ORG"          . 3)
+        ("MELPA"        . 1)))
 
 (setq package-enable-at-startup nil)
 (package-initialize)
